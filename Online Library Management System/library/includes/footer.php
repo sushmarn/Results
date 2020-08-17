9a2b59474745a7b8336cf4dafe8ef7f71ed41c74
sushma <sushmarn20@gmail.com>
2020-08-17 09:56:26 +0530

    commit

---
+++ Online Library Management System/library/includes/footer.php
@@ -0,0 +1,9 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12"> Online Library Management System  
+                </div>
+
+            </div>
+        </div>
+    </section>
